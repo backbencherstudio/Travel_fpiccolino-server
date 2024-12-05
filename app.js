68bc92b1046e5bea8f7e1a8708bcc3fc928a6e96
Seam360 <tqmhosain@gmail.com>
2024-12-05 11:36:43 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors')
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const route = require('./modules/users/users.routes');
+
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(morgan('dev'))
+
+app.use(cookieParser());
+app.use(
+    session({
+        secret: "changeit",
+        resave: true,
+        saveUninitialized: true,
+        cookie: { maxAge: 600000 },
+    })
+);
+
+
+app.use("/users", route)
+
+
+
+
+
+app.use((req, res, next) => {
+    res.status.json({
+        message: "404! Route is not found"
+    })
+})
+
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        message: "500! Something broken"
+    })
+})
+
+module.exports = app;
+
+//hello
