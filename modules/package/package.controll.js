b6a5341b9c9ceb90bfc21d4a1391a40676dada72
Tariq-Monowar <hassainseam@gmail.com>
2024-12-25 16:18:38 +0600

    new commit

--- modules/package/package.controll.js
+++ modules/package/package.controll.js
@@ -35,13 +35,14 @@ const createPackage = async (req, res) => {
 const getAllPackages = async (req, res) => {
   try {
     const packages = await Package.find();
-    res.status(200).json(packages);
+    res.status(200).json(packagesWithImageUrls);
   } catch (error) {
     res
       .status(500)
       .json({ message: "Error fetching packages", error: error.message });
   }
 };
+
 
 const getPackageById = async (req, res) => {
   try {
@@ -50,7 +51,11 @@ const getPackageById = async (req, res) => {
     if (!package) {
       return res.status(404).json({ message: "Package not found" });
     }
-    res.status(200).json(package);
+
+    res.status(200).json({
+      ...package.toObject(),
+      imageUrl: package.images.map((path) => getImageUrl(path)), // Add the image URLs dynamically
+    });
   } catch (error) {
     res
       .status(500)
@@ -58,21 +63,57 @@ const getPackageById = async (req, res) => {
   }
 };
 
+// const updatePackage = async (req, res) => {
+//   try {
+//     const packageId = req.params.id;
+//     const updatedData = req.body;
+//     const updatedPackage = await Package.findByIdAndUpdate(
+//       packageId,
+//       updatedData,
+//       { new: true }
+//     );''
+//     if (!updatedPackage) {
+//       return res.status(404).json({ message: "Package not found" });
+//     }
+//     res.status(200).json({
+//       message: "Package updated successfully",
+//       package: updatedPackage,
+//     });
+//   } catch (error) {
+//     res
+//       .status(400)
+//       .json({ message: "Error updating package", error: error.message });
+//   }
+// };
+
+
+
 const updatePackage = async (req, res) => {
   try {
     const packageId = req.params.id;
     const updatedData = req.body;
+
+ 
+    if (req.files && req.files.length > 0) {
+      updatedData.images = req.files.map((file) => `/uploads/${file.filename}`);
+    }
+
     const updatedPackage = await Package.findByIdAndUpdate(
       packageId,
       updatedData,
       { new: true }
-    );''
+    );
+
     if (!updatedPackage) {
       return res.status(404).json({ message: "Package not found" });
     }
+
     res.status(200).json({
       message: "Package updated successfully",
-      package: updatedPackage,
+      package: {
+        ...updatedPackage.toObject(),  
+        imageUrl: updatedPackage.images.map((path) => getImageUrl(path)),  
+      },
     });
   } catch (error) {
     res
@@ -80,6 +121,7 @@ const updatePackage = async (req, res) => {
       .json({ message: "Error updating package", error: error.message });
   }
 };
+
 
 const deletePackage = async (req, res) => {
   try {
