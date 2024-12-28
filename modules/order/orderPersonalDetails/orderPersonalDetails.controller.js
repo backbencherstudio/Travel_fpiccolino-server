678f0922642140c65d29b16c9bc9c8ee19ac7724
sojebsikder <sojebsikder@gmail.com>
2024-12-28 16:31:43 +0600

    added order personal details

---
+++ modules/order/orderPersonalDetails/orderPersonalDetails.controller.js
@@ -0,0 +1,68 @@
+const OrderPersonalDetails = require("./orderPersonalDetails.models");
+
+const create = async (req, res) => {
+  try {
+    const {
+      userId,
+      orderId,
+      firstName,
+      lastName,
+      email,
+      phone,
+      birthDate,
+      physicalIssues,
+    } = req.body;
+
+    const orderPersonalDetails = new OrderPersonalDetails({
+      userId,
+      orderId,
+      firstName,
+      lastName,
+      email,
+      phone,
+      birthDate,
+      physicalIssues,
+    });
+    await orderPersonalDetails.save();
+
+    res.status(201).json({
+      message: "Record created successfully",
+    });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "Error creating record", error: error.message });
+  }
+};
+
+const getAll = async (req, res) => {
+  try {
+    const categories = await OrderPersonalDetails.find();
+    res.status(200).json(categories);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching", error: error.message });
+  }
+};
+
+const deleteById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const orderPersonalDetails = await OrderPersonalDetails.findByIdAndDelete(
+      id
+    );
+    if (!orderPersonalDetails) {
+      return res.status(404).json({ message: "Record not found" });
+    }
+    res.status(200).json({ message: "Record deleted successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error deleting record", error: error.message });
+  }
+};
+
+module.exports = {
+  create,
+  getAll,
+  deleteById,
+};
