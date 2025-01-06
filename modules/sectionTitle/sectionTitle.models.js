cc173c918c25da897fd09038ddcba363d9d75a2c
sr sohan <sr.sohan088@gmail.com>
2025-01-06 16:08:14 +0600

    add main

--- modules/sectionTitle/sectionTitle.models.js
+++ modules/sectionTitle/sectionTitle.models.js
@@ -6,10 +6,24 @@ const sectinTitle = new Schema(
     title: { type: String },
     description: { type: String },
     name: {
-        type: String,
-        enum: ["landing1", "landing2", "landing3", "landing4", "landing5", "landing6", "all_inclusive_tour1", "all_inclusive_tour2", "country_wise1", "country_wise2", "about1", "about2", ],
-      },
-    pageName : {type : String, enum : ["landing", "about", "tours", "contact"]}
+      type: String,
+      enum: [
+        "landing1",
+        "landing2",
+        "landing3",
+        "landing4",
+        "landing5",
+        "landing6",
+        "all_inclusive_tour1",
+        "all_inclusive_tour2",
+        "country_wise1",
+        "country_wise2",
+        "about1",
+        "about2",
+        "about3",
+      ],
+    },
+    pageName: { type: String, enum: ["landing", "about", "tours", "contact"] },
   },
   { timestamps: true }
 );
