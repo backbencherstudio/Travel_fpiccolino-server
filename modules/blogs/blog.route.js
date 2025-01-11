0d96e25e8336f0f6c814ff56b12110609e837aad
sr sohan <sr.sohan088@gmail.com>
2025-01-11 10:21:14 +0600

    add admin

--- modules/blogs/blog.route.js
+++ modules/blogs/blog.route.js
@@ -14,7 +14,7 @@ const {
   getAllBlogsAndCategoryCount,
   getPopularBlogs,
   getBlogsGroupedByCategory,
-  updateTexContent
+  updateTexContent,
 } = require("../blogs/blog.controller");
 // const { authenticate } = require("../middlewares/authMiddleware");
 // const { uploads } = require("../config/SingleMuler.config");
@@ -22,22 +22,20 @@ const { upload } = require("../../config/Multer.config");
 const { verifyAdmin } = require("../../middleware/verifyAdmin");
 const router = express.Router();
 
-
-
 // Blog routes
-router.post('/uploads', verifyAdmin, upload.array("images"), uploadsImage);
-router.post('/createblog', verifyAdmin, createBlog);
-router.get('/blogGet/:id', getBlogById);
-router.patch('/updateHeroSection/:id', verifyAdmin, updateSpecificFields);
-router.patch('/updateContent/:id/:contentID', verifyAdmin, updateContentFields);
-router.patch('/updatecatagory/:id', verifyAdmin, UpdateCategory);
-router.patch('/updateTexContent/:id', verifyAdmin, updateTexContent);
-router.delete('/deleteContent/:id/:contentID', verifyAdmin, deleteContentAtIndex);
-router.delete('/blogDelete/:id', verifyAdmin, deleteBlog);
-router.get('/allblogs', getAllBlogs);
-router.get('/categorywise', filterBlogsByCategory);
-router.get('/categoryCount', getAllBlogsAndCategoryCount);
-router.get('/popular', getPopularBlogs);
-router.get('/all', getBlogsGroupedByCategory);
+router.post("/uploads", upload.array("images"), uploadsImage);
+router.post("/createblog", createBlog);
+router.get("/blogGet/:id", getBlogById);
+router.patch("/updateHeroSection/:id", updateSpecificFields);
+router.patch("/updateContent/:id/:contentID", updateContentFields);
+router.patch("/updatecatagory/:id", UpdateCategory);
+router.patch("/updateTexContent/:id", updateTexContent);
+router.delete("/deleteContent/:id/:contentID", deleteContentAtIndex);
+router.delete("/blogDelete/:id", deleteBlog);
+router.get("/allblogs", getAllBlogs);
+router.get("/categorywise", filterBlogsByCategory);
+router.get("/categoryCount", getAllBlogsAndCategoryCount);
+router.get("/popular", getPopularBlogs);
+router.get("/all", getBlogsGroupedByCategory);
 
 module.exports = router;
