ab37bf6972bb4bbcdc27861a08a587216416ba0c
sohan284 <sr.sohan088@gmail.com>
2025-02-23 09:41:26 +0600

    image upload

--- modules/country/country.controller.js
+++ modules/country/country.controller.js
@@ -1,5 +1,7 @@
 const { getImageUrl } = require("../../util/image_path");
 const Country = require("./country.model");
+const fs = require("fs");
+const path = require("path");
 
 const create = async (req, res) => {
   try {
@@ -71,74 +73,16 @@ const getOne = async (req, res) => {
   }
 };
 
-// const update = async (req, res) => {
-//   try {
-//     const id = req.params.id;
-//     const { name, contentTitle, contentDescription } = req.body;
-
-//     const country = await Country.findByIdAndUpdate(
-//       id,
-//       {
-//         name: name,
-//         contentTitle: contentTitle,
-//         contentDescription: contentDescription,
-//       },
-//       {
-//         new: true,
-//       }
-//     );
-//     if (!country) {
-//       return res.status(404).json({ message: "Country not found" });
-//     }
-//     res.status(200).json({
-//       message: "Country updated successfully",
-//     });
-//   } catch (error) {
-//     res
-//       .status(400)
-//       .json({ message: "Error updating country", error: error.message });
-//   }
-// };
-
-// const update = async (req, res) => {
-//   try {
-//     const id = req.params.id;
-//     const { name, contentTitle, contentDescription } = req.body;
-
-//     const updateData = {
-//       name,
-//       contentTitle,
-//       contentDescription,
-//     };
-
-//     if (req.file) {
-//       updateData.image = `/uploads/${req.file.filename}`;
-//     }
-
-//     const country = await Country.findByIdAndUpdate(id, updateData, {
-//       new: true,
-//     });
-
-//     if (!country) {
-//       return res.status(404).json({ message: "Country not found" });
-//     }
-
-//     res.status(200).json({
-//       message: "Country updated successfully",
-//       country: {
-//         ...country.toObject(),
-//         image: getImageUrl(country.image),
-//       },
-//     });
-//   } catch (error) {
-//     res.status(400).json({ message: "Error updating country", error: error.message });
-//   }
-// };
-
 const update = async (req, res) => {
   try {
     const id = req.params.id;
     const { name, contentTitle, contentDescription } = req.body;
+
+    // First get the existing country to check for previous image
+    const existingCountry = await Country.findById(id);
+    if (!existingCountry) {
+      return res.status(404).json({ message: "Country not found" });
+    }
 
     const updateData = {
       name,
@@ -147,16 +91,23 @@ const update = async (req, res) => {
     };
 
     if (req.file) {
+      // If there's a previous image, delete it
+      if (existingCountry.image) {
+        const previousImagePath = path.join(
+          __dirname,
+          "../../",
+          existingCountry.image
+        );
+        if (fs.existsSync(previousImagePath)) {
+          fs.unlinkSync(previousImagePath);
+        }
+      }
       updateData.image = `/uploads/${req.file.filename}`;
     }
 
     const country = await Country.findByIdAndUpdate(id, updateData, {
       new: true,
     });
-
-    if (!country) {
-      return res.status(404).json({ message: "Country not found" });
-    }
 
     res.status(200).json({
       message: "Country updated successfully",
