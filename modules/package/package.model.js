051a0410e5615ae9750623887227a20f56bef57b
sohan284 <sr.sohan088@gmail.com>
2025-04-09 16:44:41 +0600

    add timer

--- modules/package/package.model.js
+++ modules/package/package.model.js
@@ -14,6 +14,10 @@ const packageSchema = new mongoose.Schema(
       type: Date,
       // required: true,
     },
+    offerEndsIn: {
+      type: Date,
+      // required: true,
+    },
     tourDuration: {
       nights: {
         type: Number,
